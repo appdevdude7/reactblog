122331402d587bb1ca3468bbc2b8f25cd9ece49d
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-15 11:58:07 -0600

    created singlepost and Posts components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Posts from './components/Posts';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <Header />
       </header>
+      <Posts />
     </div>
   );
 }
---
+++ src/components/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SinglePost from './SinglePost'
+
+const Posts = () => {
+    return (
+        <div>
+            <SinglePost />
+        </div>
+    )
+}
+
+export default Posts
