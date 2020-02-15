e2f752c66564880957737df5e8389a81a4a26459
appdevdude7 <appdeveloper.akshay@gmail.com>
2020-02-15 11:50:36 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <Header />
       </header>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+          <ul className="menu-item">
+              <li className="menu-list">Home</li>
+              <li className="menu-list">Blog</li>
+              <li className="menu-list">Single Post</li>
+          </ul>
+        </>
+    )
+}
+
+export default Header
